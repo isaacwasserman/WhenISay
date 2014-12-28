771146ca8cf24dbe9ea72f888171aeff70a06e2f
Isaac Wasserman <iwasserblaster@gmail.com>
2014-12-27 23:44:18 -0500

    Added Messaging Component

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 var express = require('express');
 var app = express();
+
+var bodyParser = require('body-parser');
+
 var cool = require('cool-ascii-faces');
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'sumworldmyworld@gmail.com',
+    pass: 'kinnock2013'
+  }
+});
+
+
+
 
 app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
+app.post('/submit', function(req, res) {
+  console.log('SUBMITTING');
+  console.log(req.body);
+  res.redirect('/');
+  
+  var mailOptions = {
+    from: 'Fred Foo ✔ <whenisayinput@raritea.com>', // sender address
+    to: 'whenisayinput@raritea.com', // list of receivers
+    subject: 'New Input On When I Say!', // Subject line
+    text: "Adjective: " + req.body.adjective + " Noun: " + req.body.noun, // plaintext body
+    html: '<b>Adjective: </b>' + req.body.adjective + ' <b>Noun: </b>' + req.body.noun // html body
+  };
+});
+
 app.get('/', function(request, response) {
-  response.send('/public/index.html');
+  // send mail with defined transport object
+//  transporter.sendMail(mailOptions, function(error, info){
+//    console.log('Mail callback');
+//      if(error){
+//          console.log(error);
+//      }else{
+//          console.log('Message sent: ' + info.response);
+//      }
+//  });
+  
+  response.sendFile(__dirname + '/public/main.html');
+  
 });
 
 app.listen(app.get('port'), function() {
