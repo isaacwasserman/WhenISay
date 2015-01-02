8ced3bcc00a4f61c6e8eb16765b4b21e9422de81
Isaac Wasserman <iwasserblaster@gmail.com>
2015-01-02 18:09:53 -0500

    ...

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ app.post('/submit', function(req, res) {
   res.redirect('/');
   
   var emailtext = "Someone just entered new words on whenisay.com! Adjective: " + req.body.adjective + " Noun: " + req.body.noun;
-  var emailhtml = 'Someone just entered new words on whenisay.com!<br><span style="font-size:24px"><b>Adjective: </b></span>' + req.body.adjective + '<br><span style="font-size:24px"><b>Noun: </b></span>' + req.body.noun;
+  //var emailhtml = 'Someone just entered new words on whenisay.com!<br><span style="font-size:24px"><b>Adjective: </b></span>' + req.body.adjective + '<br><span style="font-size:24px"><b>Noun: </b></span>' + req.body.noun + '<br><br>View this WhenISay at: <a href="http://www.whenisay.com?adjective='req.body.adjective'&noun='req.body.noun'"></a>';
   
   var mailOptions = {
-    from: 'Fred Foo ✔ <whenisayinput@raritea.com>', // sender address
+    from: 'When I Say... <whenisayinput@raritea.com>', // sender address
     to: 'whenisayinput@raritea.com', // list of receivers
     subject: 'New Input On When I Say!', // Subject line
     text: emailtext, // plaintext body
